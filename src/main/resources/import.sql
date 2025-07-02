f32db8fa35d919ab320298a6f2e98047ddac6f31
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-07-02 16:24:54 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$QkOn3KFLPVK5Hq3BmhlG4OgWd/Gy2LgUs.H2uepTgYdfZRyOa6t.W');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$QkOn3KFLPVK5Hq3BmhlG4OgWd/Gy2LgUs.H2uepTgYdfZRyOa6t.W');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
